3cf136d3673de62e1d4410d908ce5a656b466bad
Francisco Aguirre <franmaguirre@gmail.com>
2023-05-21 11:42:38 -0300

    crear-tarjeta-component

--- src/app/components/crear-tarjeta/crear-tarjeta.component.ts
+++ src/app/components/crear-tarjeta/crear-tarjeta.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-crear-tarjeta',
@@ -7,9 +8,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CrearTarjetaComponent implements OnInit {
 
-  constructor() { }
+  form: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      titular: ['', Validators.required],
+      numeroTarjeta: ['', [Validators.required, Validators.minLength(16), Validators.maxLength(16)]],
+      fechaExpiracion: ['', [Validators.required, Validators.minLength(5), Validators.maxLength(5)]],
+      cvv: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(3)]]
+    })
+  }
 
   ngOnInit(): void {
   }
+
+
+  crearTarjeta() {
+    console.log(this.form.value);
+  }
+
+
+
 
 }
